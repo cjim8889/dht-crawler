97ede97c191fc65226b26435a6a19a99875600c2
cjim8889 <43540548+cjim8889@users.noreply.github.com>
2023-06-24 22:22:31 +0000

    Change main.go

--- main.go
+++ main.go
@@ -21,17 +21,14 @@ import (
 	"github.com/Wessie/appdirs"
 )
 
-type opFlags struct {
-	DatabaseURL string
-
-	IndexerAddrs        []string
-	IndexerInterval     time.Duration
-	IndexerMaxNeighbors uint
-
-	LeechMaxN int
-
-	Verbosity int
-	Profile   string
+type opts struct {
+	DatabaseURL         string        `long:"database" env:"DATABASE_URL" description:"URL of the database."`
+	IndexerAddrs        []string      `long:"indexer-addr" env:"INDEXER_ADDR" default:"0.0.0.0:0" description:"Address(es) to be used by indexing DHT nodes."`
+	IndexerInterval     time.Duration `long:"indexer-interval" env:"INDEXER_INTERVAL" default:"1s" description:"Indexing interval in integer seconds."`
+	IndexerMaxNeighbors uint          `long:"indexer-max-neighbors" env:"INDEXER_MAX_NEIGHBORS" default:"1000" description:"Maximum number of neighbors of an indexer."`
+	LeechMaxN           uint          `long:"leech-max-n" env:"LEECH_MAX_N" default:"50" description:"Maximum number of leeches."`
+	Verbosity           int           `long:"verbose" env:"VERBOSITY" default:"1" description:"Increases verbosity."`
+	Profile             string        `long:"profile" env:"PROFILE" description:"Enable profiling."`
 }
 
 var compiledOn string
@@ -95,7 +92,7 @@ func main() {
 	}
 
 	trawlingManager := dht.NewManager(opFlags.IndexerAddrs, opFlags.IndexerInterval, opFlags.IndexerMaxNeighbors)
-	metadataSink := metadata.NewSink(5*time.Second, opFlags.LeechMaxN)
+	metadataSink := metadata.NewSink(5*time.Second, opFlags.Verbosity)
 
 	// The Event Loop
 	for stopped := false; !stopped; {
@@ -129,29 +126,15 @@ func main() {
 	}
 }
 
-func parseFlags() (*opFlags, error) {
-	var cmdF struct {
-		DatabaseURL string `long:"database" description:"URL of the database."`
-
-		IndexerAddrs        []string `long:"indexer-addr" description:"Address(es) to be used by indexing DHT nodes." default:"0.0.0.0:0"`
-		IndexerInterval     uint     `long:"indexer-interval" description:"Indexing interval in integer seconds." default:"1"`
-		IndexerMaxNeighbors uint     `long:"indexer-max-neighbors" description:"Maximum number of neighbors of an indexer." default:"1000"`
-
-		LeechMaxN uint `long:"leech-max-n" description:"Maximum number of leeches." default:"50"`
-
-		Verbose []bool `short:"v" long:"verbose" description:"Increases verbosity."`
-		Profile string `long:"profile" description:"Enable profiling." choice:"cpu" choice:"memory"`
-	}
-
-	opF := new(opFlags)
-
-	_, err := flags.Parse(&cmdF)
+func parseFlags() (*opts, error) {
+	o := &opts{}
+	_, err := flags.Parse(o)
 	if err != nil {
 		return nil, err
 	}
 
-	if cmdF.DatabaseURL == "" {
-		opF.DatabaseURL =
+	if o.DatabaseURL == "" {
+		o.DatabaseURL =
 			"sqlite3://" +
 				appdirs.UserDataDir("magneticod", "", "", false) +
 				"/database.sqlite3" +
@@ -159,32 +142,20 @@ func parseFlags() (*opFlags, error) {
 				"&_busy_timeout=3000" + // in milliseconds
 				"&_foreign_keys=true"
 
-	} else {
-		opF.DatabaseURL = cmdF.DatabaseURL
 	}
 
-	if err = checkAddrs(cmdF.IndexerAddrs); err != nil {
+	if err = checkAddrs(o.IndexerAddrs); err != nil {
 		zap.S().Fatalf("Of argument (list) `trawler-ml-addr`", zap.Error(err))
-	} else {
-		opF.IndexerAddrs = cmdF.IndexerAddrs
 	}
 
-	opF.IndexerInterval = time.Duration(cmdF.IndexerInterval) * time.Second
-	opF.IndexerMaxNeighbors = cmdF.IndexerMaxNeighbors
-
-	opF.LeechMaxN = int(cmdF.LeechMaxN)
-	if opF.LeechMaxN > 1000 {
+	if o.LeechMaxN > 1000 {
 		zap.S().Warnf(
 			"Beware that on many systems max # of file descriptors per process is limited to 1024. " +
 				"Setting maximum number of leeches greater than 1k might cause \"too many open files\" errors!",
 		)
 	}
 
-	opF.Verbosity = len(cmdF.Verbose)
-
-	opF.Profile = cmdF.Profile
-
-	return opF, nil
+	return o, nil
 }
 
 func checkAddrs(addrs []string) error {
